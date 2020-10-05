26c86971c400a51e26a7b394853f684dd816dc2d
felixyangyicheng <felix.yang.yicheng@gmail.com>
2020-10-05 14:44:45 +0200

    ADD game rules and results

---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPShyFuMyServer
+{
+    public class Player
+    {
+
+        public int GameReceive { get; set; }
+
+
+        public static int? GamePlay(int element)
+            {
+                switch (element)
+                {
+                    case 1/*"feuille"*/: break;
+                    case 2/*"Pierre"*/: break;
+                    case 3/*"Ciseux"*/: break;
+                    
+                    default: return null;
+                }
+
+            return element;
+            }
+        
+    }
+}
