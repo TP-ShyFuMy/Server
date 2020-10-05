ec95ea59cbd222c4d1749962565d203729af992c
Miguel-Gremy <miguel.gremy72@gmail.com>
2020-10-05 13:40:59 +0200

    NetSocket setup

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using SockNet.ServerSocket;
 
 namespace TPShyFuMyServer
 {
@@ -7,6 +13,26 @@ namespace TPShyFuMyServer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Server!");
+            var server = new SocketServer();
+            server.InitializeSocketServer("0.0.0.0", 80);
+            server.SetReaderBufferBytes(2048);
+            server.StartListening();
+            bool openServer = true;
+            while (openServer)
+            {
+                if (server.IsNewData())
+                {
+                    var data = server.GetData();
+                    Task.Run(() => PrintRequest(data, server));
+                }
+            }
+        }
+
+        private static void PrintRequest(KeyValuePair<TcpClient, byte[]> data, SocketServer server)
+        {
+            Console.WriteLine(((IPEndPoint) data.Key.Client.RemoteEndPoint).Address +
+                              ": " + Encoding.UTF8.GetString(data.Value));
+            server.ResponseToClient(data.Key, "received");
         }
     }
-}
+}g
