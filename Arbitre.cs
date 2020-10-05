69b13c8400255ec973973168ac218dee966641d0
felixyangyicheng <felix.yang.yicheng@gmail.com>
2020-10-05 14:44:45 +0200

    ADD game rules and results

---
+++ Arbitre.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPShyFuMyServer
+{
+    public class Arbitre
+    {
+        
+
+
+            public static string WhoWin(int playerAGameplay, int playerBGamplay)
+            {
+
+                if (playerAGameplay == 1 && playerBGamplay == 2 || playerAGameplay == 2 && playerBGamplay == 3 || playerAGameplay == 3 && playerBGamplay == 1)
+                {
+                    return "Player A Win";
+                }
+                else if (playerAGameplay == playerBGamplay)
+                {
+                    return "Deuce";
+
+                }
+
+                else
+                {
+                    return "Player B Win";
+                }
+
+            }
+        
+    }
+}
